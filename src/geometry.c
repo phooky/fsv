0f1a3b6e956eedecda29c6cb90840780960cc0d1
Janne Blomqvist <blomqvist.janne@gmail.com>
2021-11-07 17:49:49 +0200

    Remove outlining for TREEV mode

--- src/geometry.c
+++ src/geometry.c
@@ -43,8 +43,6 @@ static int fstree_high_draw_stage;
 
 
 /* Forward declarations */
-static void outline_pre( void );
-static void outline_post( void );
 static void cursor_pre( void );
 static void cursor_hidden_part( void );
 static void cursor_visible_part( void );
@@ -2765,11 +2763,6 @@ treev_draw( boolean high_detail )
 	if (high_detail) {
 		/* Draw additional high-detail stuff */
 
-		/* "Cel lines" */
-		outline_pre();
-		treev_draw_recursive(globals.fstree, NIL, treev_core_radius, TREEV_DRAW_GEOMETRY);
-		outline_post();
-
 		/* Node name labels */
 		text_pre();
 		treev_draw_recursive(globals.fstree, NIL, treev_core_radius, TREEV_DRAW_LABELS);
@@ -2785,24 +2778,6 @@ treev_draw( boolean high_detail )
 
 
 /**** COMMON ROUTINES *****************************************/
-
-
-/* Call before drawing geometry "cel lines" */
-static void
-outline_pre( void )
-{
-	glDisable( GL_CULL_FACE );
-	glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
-}
-
-
-/* Call after drawing "cel lines" */
-static void
-outline_post( void )
-{
-	glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
-	glEnable( GL_CULL_FACE );
-}
 
 
 /* Call before drawing the cursor */
