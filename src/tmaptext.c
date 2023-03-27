3db3d2a79108c55b66168a6a6889c9069a778693
Adam Mayer <phooky@gmail.com>
2023-03-27 16:11:09 -0400

    replace GL_CLAMP with GL_CLAMP_TO_EDGE
    GL_CLAMP has been deprecated for a while; you'll need to use GL_CLAMP_TO_EDGE instead.

--- src/tmaptext.c
+++ src/tmaptext.c
@@ -169,8 +169,8 @@ text_init( void )
 	glBindTexture( GL_TEXTURE_2D, text_tobj );
 
 	/* Set up texture-mapping parameters */
-	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP );
-	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP );
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
 	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR );
 
 	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
